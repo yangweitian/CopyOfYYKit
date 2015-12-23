dd6890331f9d207051bcb9a17d14a68d1b584dc9
YangWeitian <yangweitian@yangweitiandemacbook-air.local>
2015-12-23 10:50:49 +0800

    Initial Commit

---
+++ YYKit_ywt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YYKit_ywt
+//
+//  Created by YangWeitian on 15/12/23.
+//  Copyright © 2015年 YangWeitian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YYKit_ywt/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YYKit_ywt
+//
+//  Created by YangWeitian on 15/12/23.
+//  Copyright © 2015年 YangWeitian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
